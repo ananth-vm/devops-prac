a9e5a370c3625c4746e7d5116650eb98c3e030f5
JEEVANANTHAN K <jeevananthan8507@gmail.com>
2026-08-19 13:56:04 +0530

    Refactor Jenk class to output build success message

--- Jenk.java
+++ Jenk.java
@@ -1,5 +1,8 @@
-public class Jenk {
-  public static void main(String[] args) {
-    System.out.println("Running...");
-  }
+import java.time.LocalDateTime;
+
+public class jenk {
+    public static void main(String[] args) {
+        System.out.println("Jenkins build executed successfully!");
+        System.out.println("Execution time: " + LocalDateTime.now());
+    }
 }
